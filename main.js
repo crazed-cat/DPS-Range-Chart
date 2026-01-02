6423f16c208413cabff55787080af62ec75f98f1
crazed-cat <huangshanyu56@gmail.com>
2026-01-02 19:10:15 +0800

    imports the data from the sheet

--- main.js
+++ main.js
@@ -1,68 +1,10 @@
 google.charts.load('current', { packages: ['corechart'] });
 google.charts.setOnLoadCallback(init);
 
-// 內建測資
-  const datasets = {
-  none: [
-    ['label', 'x', 'y1', 'y2', 'y3','有料?'],
-    ['p1', 1, 10, 20, 30, 0],
-    ['p2', 2, 15, 25, 35, 0],
-    ['p3', 3, 5,  30, 40, 1]
-  ],
-  red: [
-    ['label', 'x', 'y1', 'y2', 'y3','有料?'],
-    ['q1', 1, 50, 10, 5,0],
-    ['q2', 2, 40, 20, 15,1],
-    ['q3', 3, 30, 25, 20,0]
-  ],
-  float: [
-    ['label', 'x', 'y1', 'y2', 'y3','有料?'],
-    ['r1', 1, 5,  60, 10,0],
-    ['r2', 2, 10, 55, 20,1],
-    ['r3', 3, 15, 50, 30,1]
-  ],
-  black: [
-    ['label', 'x', 'y1', 'y2', 'y3','有料?'],
-    ['p1', 1, 10, 20, 30,1],
-    ['p2', 2, 15, 25, 35,0],
-    ['p3', 3, 5,  30, 40,0]
-  ],
-  angle: [
-    ['label', 'x', 'y1', 'y2', 'y3','有料?'],
-    ['q1', 1, 50, 10, 5,1],
-    ['q2', 2, 40, 20, 15,0],
-    ['q3', 3, 30, 25, 20,1]
-  ],
-  alien: [
-    ['label', 'x', 'y1', 'y2', 'y3','有料?'],
-    ['r1', 1, 5,  60, 10,1],
-    ['r2', 2, 10, 55, 20,1],
-    ['r3', 3, 15, 50, 30,0]
-  ],
-  zombie: [
-    ['label', 'x', 'y1', 'y2', 'y3','有料?'],
-    ['p1', 1, 10, 20, 30,0],
-    ['p2', 2, 15, 25, 35,1],
-    ['p3', 3, 5,  30, 40,1]
-  ],
-  relic: [
-    ['label', 'x', 'y1', 'y2', 'y3','有料?'],
-    ['q1', 1, 50, 10, 5,0],
-    ['q2', 2, 40, 20, 15,1],
-    ['q3', 3, 30, 25, 20,0]
-  ],
-  aku: [
-    ['label', 'x', 'y1', 'y2', 'y3','有料?'],
-    ['r1', 1, 5,  60, 10,1],
-    ['r2', 2, 10, 55, 20,0],
-    ['r3', 3, 15, 50, 30,1]
-  ],
-  traitless: [
-    ['label', 'x', 'y1', 'y2', 'y3','有料?'],
-    ['r1', 1, 5,  60, 10,1],
-    ['r2', 2, 10, 55, 20,1],
-    ['r3', 3, 15, 50, 30,0]
-  ]
+// 改為引入外部資料
+const datasets = {
+  none: '0', red: '1438024126', float: '545386183', black: '1326931744', angle: '265711189',
+  alien: '1362318074', zombie:'205622714', relic: '2039091253', aku: '2140034265', traitless: '1738351081'
 };
 
 let chart;
@@ -139,14 +81,23 @@ function applyFilter() {
 }
 
 function loadTable(key) {
-  data = google.visualization.arrayToDataTable(datasets[key]);
+  const query = new google.visualization.Query(
+    `1A6OllbUHCiVlk_gbyYRW2JkNIGpuqvv8oRGsTT-Nh0w?gid=${sheetMap[key]}`
+  );
 
-  view = new google.visualization.DataView(data);
-  view.setColumns([
-    1, // X
-    2, { type: 'string', role: 'tooltip', calc: (dt, row) => `${dt.getValue(row,0)}` },
-    3, { type: 'string', role: 'tooltip', calc: (dt, row) => `${dt.getValue(row,0)}` },
-    4, { type: 'string', role: 'tooltip', calc: (dt, row) => `${dt.getValue(row,0)}` }
-  ]);
-  //alert('loaded')
+  query.send(function (response) {
+    if (response.isError()) {
+      console.error(response.getMessage());
+      return;
+    }
+  
+    data = response.getDataTable();
+    view = new google.visualization.DataView(data);
+    view.setColumns([
+      1, // X
+      2, { type: 'string', role: 'tooltip', calc: (dt, row) => `${dt.getValue(row,0)}` },
+      3, { type: 'string', role: 'tooltip', calc: (dt, row) => `${dt.getValue(row,0)}` },
+      4, { type: 'string', role: 'tooltip', calc: (dt, row) => `${dt.getValue(row,0)}` }
+    ]);
+  });
 }
