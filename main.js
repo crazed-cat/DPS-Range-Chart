c8f005915849649f9811f0d6e29efa84543b392b
crazed-cat <huangshanyu56@gmail.com>
2026-03-17 17:18:34 +0800

    優化算法

--- main.js
+++ main.js
@@ -1,104 +1,87 @@
 google.charts.load('current', { packages: ['corechart'] });
-google.charts.setOnLoadCallback(init);
+google.charts.setOnCallBack(init);
 
 // 外部資料
-const dataset = {
+const sheetId = {
   none: '0', red: '1438024126', float: '545386183', black: '1326931744', angle: '265711189',
   alien: '1362318074', zombie:'205622714', relic: '2039091253', aku: '2140034265', traitless: '1738351081'
 };
 
 let chart;
-let data;
+let data= new Map();
 let view;
 let options;
 
-function init() {
-  loadTable('none')
+function loadTable() {
+  const key =  document.getElementById('traitSelector').value
+  if (!data.has(key)){
+    const query = new google.visualization.Query(
+    	`1A6OllbUHCiVlk_gbyYRW2JkNIGpuqvv8oRGsTT-Nh0w?gid=${sheetId[key]}`
+  	);
 
-  //alert('check')
+  	query.send(function (response) {
+    	if (response.isError()) {
+      	console.error(response.getMessage());
+      	return;
+    	}
+  
+    	data.set (key, response.getDataTable());
+  	);
+  }
+  
+  view = new google.visualization.DataView(data.get(key));
+  view.setColumns([
+    2, // X
+    3, { type: 'string', role: 'tooltip', calc: (dt, row) => `${dt.getValue(row,1)}` },
+    4, { type: 'string', role: 'tooltip', calc: (dt, row) => `${dt.getValue(row,1)}` },
+    5, { type: 'string', role: 'tooltip', calc: (dt, row) => `${dt.getValue(row,1)}` }
+  ]);
+  
+  if (document.getElementById('metaOnly').checked){
+    const rows = [];
 
+  	for (let i = 0; i < data.getNumberOfRows(); i++) {
+    	if (data.getValue(i, 6) == 1) {rows.push(i);}
+  	}
+    
+    view.setRows(rows);
+  }
+  chart.draw(view, options);
+}
+
+
+function init(){
   options = {
     title: 'DPS-射程 比較表',
     hAxis: { title: '射程(接觸點)' },
     vAxis: { title: 'DPS(秒平均輸出)', logScale:'false' },
-    legend: { position: 'right' },
-    height: 700,
+    legend: { position: 'top' },
     explorer: {
       actions: ['scrollToZoom', 'dragToPan', 'rightClickToReset'],
       axis: 'both',
       keepInBounds: true,
       maxZoomIn: 0.1, maxZoomOut:1.2, zoomDelta:1.2
     }
-  };
+  };//初始化選項
 
   chart = new google.visualization.ScatterChart(
     document.getElementById('chart_div')
-  );
+  );//綁定圖表
 
-  // UI 綁定
-  document
-    .getElementById('logToggle')
-    .addEventListener('change', toggleLogScale);
-
-  document
-    .getElementById('metaOnly')
-    .addEventListener('change', function(){
-      applyFilter();
-      chart.draw(view, options);
-    })
-
-  document
-    .getElementById('traitSelector')
-    .addEventListener('change',function(){
-      //alert(this.value)
-      loadTable(this.value)
-      applyFilter();
-      chart.draw(view, options);
-    })
-
-  chart.draw(view, options);
+	loadTable();
 }
 
-function toggleLogScale() {
-  options.vAxis.logScale =
-    document.getElementById('logToggle').checked;
-  
-  chart.draw(view, options);
-}
-
-function applyFilter() {
-  filter=document.getElementById('metaOnly').checked
-  ? 1:0;
-  const rows = [];
-
-  for (let i = 0; i < data.getNumberOfRows(); i++) {
-    if (data.getValue(i, 5) >= filter) {
-      rows.push(i);
-    }
-  }
-
-  view.setRows(rows);
-}
-
-function loadTable(key) {
-  const query = new google.visualization.Query(
-    `1A6OllbUHCiVlk_gbyYRW2JkNIGpuqvv8oRGsTT-Nh0w?gid=${dataset[key]}`
-  );
-
-  query.send(function (response) {
-    if (response.isError()) {
-      console.error(response.getMessage());
-      return;
-    }
-  
-    data = response.getDataTable();
-    view = new google.visualization.DataView(data);
-    view.setColumns([
-      1, // X
-      2, { type: 'string', role: 'tooltip', calc: (dt, row) => `${dt.getValue(row,0)}` },
-      3, { type: 'string', role: 'tooltip', calc: (dt, row) => `${dt.getValue(row,0)}` },
-      4, { type: 'string', role: 'tooltip', calc: (dt, row) => `${dt.getValue(row,0)}` }
-    ]);
+document
+  .getElementById('logToggle')
+  .addEventListener('change', function(){
+    options.vAxis.logScale = document.getElementById('logToggle').checked;
+    chart.draw(view, options);
   });
-}
 
+document
+  .getElementById('metaOnly')
+  .addEventListener('change', loadTable);
+
+document
+  .getElementById('traitSelector')
+  .addEventListener('change', loadTable);
